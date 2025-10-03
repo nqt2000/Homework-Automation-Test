c86caf3e82339af8022b0fb72e884cabe02315fe
Nguyen Quang Tuan <77141971+nqt2000@users.noreply.github.com>
2025-10-03 15:10:47 +0700

    Add Bai 4

---
+++ src/main/java/Bai4.java
@@ -0,0 +1,43 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Bai4 {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/checkbox");
+
+        // Locate dung ancestor
+        WebElement checkboxIcon = driver.findElement(
+                By.xpath("//span[text()='Home']/ancestor::label/span[@class='rct-checkbox']")
+        );
+        checkboxIcon.click();
+        Thread.sleep(3000);
+
+        WebElement homeToggle = driver.findElement(
+                By.xpath("//span[text()='Home']/ancestor::span[@class='rct-text']/button")
+        );
+        homeToggle.click();
+        Thread.sleep(3000);
+
+        // Locate nut mui ten cua Documents
+        WebElement toggleBtn = driver.findElement(
+                By.xpath("//span[text()='Documents']/ancestor::label/preceding-sibling::button")
+        );
+        toggleBtn.click();
+        Thread.sleep(3000);
+
+        // checkbox “Downloads” va di nguoc len
+        WebElement downloadsCheckbox = driver.findElement(
+                By.xpath("//span[text()='Downloads']/ancestor::label/span[@class='rct-checkbox']")
+        );
+        downloadsCheckbox.click();
+        Thread.sleep(5000);
+
+        driver.quit();
+    }
+}
